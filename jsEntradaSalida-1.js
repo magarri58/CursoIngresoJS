f55e3b59f250bf45aff98a977c7315c5666f1b9e
magarri58 <52534694+magarri58@users.noreply.github.com>
2019-07-04 11:22:56 -0300

    EJERCICIOS 1 Y 2.

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,11 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{
+  edad=prompt("ingrese su edad")
+	alert(newFunction());
+
+  function newFunction() {
+    return "okay";
+  }
+}
+
---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,9 @@
+/*Debemos lograr tomar un nombre con 'prompt'
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+  edad=prompt("Ingrese su edad")
+  alert("Ingresado")
+
+}
+
