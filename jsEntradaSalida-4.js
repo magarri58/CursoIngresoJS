dd8d34bfe58f0d9be5fc2a27cc81c3da58773dec
magarri58 <52534694+magarri58@users.noreply.github.com>
2019-07-04 12:14:53 -0300

    Ejercicios 3-4-5

--- jsEntradaSalida-4.js
+++ jsEntradaSalida-4.js
@@ -1,9 +1,10 @@
-/* 
-	Debemos lograr tomar un dato por 'PROMPT' 
+/*
+	Debemos lograr tomar un dato por 'PROMPT'
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
+  dato=prompt("ingreseSuNombre")
 
 }
 
