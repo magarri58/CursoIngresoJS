711e8e4d9fc5fc6bb13abef8b6a5a663148cd010
magarri58 <52534694+magarri58@users.noreply.github.com>
2019-07-06 12:44:29 -0300

    ejercicios 9-10

--- jsEntradaSalida-10.js
+++ jsEntradaSalida-10.js
@@ -4,5 +4,28 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var valorImporte;
+	var ValorResultado;
+	/*valorImporte="lalala";
+	alert(valorImporte);*/
+	valorImporte=document.getElementById('importe').value;
+	console.info("el.valor",valorImporte);
+	//alert(valorImporte);
+
+	var datoUno;
+	var datoDos;
+	var suma;
+	datoUno=prompt();
+	datoDos=document.
 	
+	datoUno =parseInt(datoUno);
+	datoDos=parseInt(datoDos);
+	
+	suma=datoUno+datoDos;
+	alert("la suma
+
+	ValorResultado=valorImporte*100;
+	//alert(ValorResultado);
+	document.getElementById('resultado').value=ValorResultado;
+
 }
--- jsEntradaSalida-9.js
+++ jsEntradaSalida-9.js
@@ -4,6 +4,35 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var valorImporte;
+    var valorResultado;
 
-	
+    valorImporte=document.getElementById('sueldo').value;
+    console.log(valorImporte);//para el programador
+    valorImporte=parseInt(valorImporte);
+    sueldo=parseInt(sueldo);
+     
+    var valorImporte;
+    var valorResultado;
+    var porcentaje;
+    
+    valorImporte =parseINT(valorImporte);
+    valorResultado=parseINT(valorResultado);
+
+    porcentaje=valorImporte*valorResultado;
+    alert("")
+    
+
+
+    valorImporte=prompt;
+    valorResultado=document;
+
+
+    
+    console.log(valorImporte);
+
+    document.getElementById('resultado').value=valorImporte;
+
+
+
 }
